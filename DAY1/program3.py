fd30093c698c064dcfb62230024019e2d7ab830e
Divya Agrawal <66077907+divyaagrawal2301@users.noreply.github.com>
2020-05-28 21:13:09 +0530

    Create program3.py

---
+++ DAY1/program3.py
@@ -0,0 +1,8 @@
+n1= int(input("Input value of n1 : "))
+n2= int(input("Input value of n2 :"))
+n1= n1+n2
+n2= n1-n2
+n1= n1-n2
+print("After swapinng:\n")
+print("Value of n1 is: ",n1)
+print("Value of n2 is: ",n2)
