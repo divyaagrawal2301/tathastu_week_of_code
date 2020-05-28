df79c27e320cdad10033b19864c191a21ee8d022
Divya Agrawal <66077907+divyaagrawal2301@users.noreply.github.com>
2020-05-28 21:13:50 +0530

    Create program4.py

---
+++ DAY1/program4.py
@@ -0,0 +1,6 @@
+cp= float(input("Enter Cost Price : "))
+sp= float(input("Enter Selling Price: "))
+profit= sp-cp
+new_sp= 1.05 * profit + cp
+print("The prifit will  be: ",profit)
+print("As per to incease profit by 5%,new selling price: ",new_sp)
